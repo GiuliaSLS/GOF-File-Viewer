5680d6845098d29f65dc61dc768808fb2f505011
Arnau Mora <arnyminerz@proton.me>
2025-10-08 07:45:49 +0200

    Initialize FileKit

--- composeApp/src/jvmMain/kotlin/com/arnyminerz/parquet_viewer/main.kt
+++ composeApp/src/jvmMain/kotlin/com/arnyminerz/parquet_viewer/main.kt
@@ -2,12 +2,18 @@ package com.arnyminerz.parquet_viewer
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.github.vinceglb.filekit.FileKit
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Parquet Viewer",
-    ) {
-        App()
+fun main() {
+    // Initialize FileKit
+    FileKit.init(appId = "ParquetViewer")
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Giulia GOF Viewer",
+        ) {
+            App()
+        }
     }
 }
